1c010e78a309c2e2e24da004fc90322d0486a1f3
노서영 <hello.syeong@netivered.com>
2021-11-04 13:57:54 +0900

    ~end

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navigation from "./components/Navigation";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 // import Home from "./routes/Home";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   )
 }
